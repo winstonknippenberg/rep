c929c6a02515265f64261189eeff2b7ede691565
Ella Tal <82758785+winstonknippenberg@users.noreply.github.com>
2021-09-09 18:22:31 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  getStuff() {
+  }
+
+  getMorningGreeting() {
+  }
+
+  getEveningGreeting() {
+  }
+
+  showCompany(name, revenue) {
+  }
+
+  getClassName(temperature) {
+
+  }
+
+  render() {
+
+    let companies = [
+      { name: "Tesla", revenue: 140 },
+      { name: "Microsoft", revenue: 300 },
+      { name: "Google", revenue: 600 }
+    ]
+
+    return (
+      <div>
+        <div className="ex-space">
+          <h4 className='ex-title'>Spot-check 1</h4 >
+          <div className="exercise" id="spotcheck-1">
+            {/* your code here */}
+          </div>
+        </div>
+
+        <div className="ex-space">
+          <h4 className='ex-title'>Spot-check 2</h4>
+          <div className="exercise" id="spotcheck-2">
+            {/* your code here */}
+          </div>
+        </div>
+
+        <div className="ex-space">
+          <h4 className='ex-title'>Spot-check 3</h4>
+          <div className="exercise" id="spotcheck-3">
+            {/* your code here */}
+          </div>
+        </div>
+
+        <div className="ex-space">
+          <h4 className='ex-title'>Exercise 1</h4>
+          <div className="exercise" id="ex-1">
+            {/* your code here */}
+          </div>
+        </div>
+
+        <div className="ex-space">
+          <h4 className='ex-title'>Exercise 2</h4>
+          <div className="exercise" id="ex-2">
+            {/* your code here */}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
